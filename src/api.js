1cc952c895f363efd61a6df0875c809582caa45b
Martins Oloyede <oloyedemartins15@gmail.com>
2023-09-11 14:22:30 +0100

    added all files

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '606905c922mshed028b6f51a557bp16e7e4jsnc681946c3a4c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "da81b66bcbf91e3b39e2796f457ea634"
